d7a1c9141ce9156d6f3de27ef4fe17f8f1fd18cc
Brendon Smith <br3ndonland@protonmail.com>
2020-07-19 20:41:02 -0400

    Add test cases and return types
    - Add test cases for palindrome.py
    - Add return statements, which are necessary for passing Mypy checks

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,5 +1,6 @@
 from examples import __version__
 
 
-def test_version():
+def test_version() -> str:
     assert __version__ == "0.1.0"
+    return __version__
--- tests/test_palindrome.py
+++ tests/test_palindrome.py
@@ -1,17 +1,33 @@
+import pytest
+
 from examples import palindrome
 
 
-def test_palindrome_01():
-    assert palindrome.palindrome("racecar") is True
+def test_palindrome_01() -> bool:
+    test = palindrome.prep_input("racecar")
+    assert test is True
+    return test
 
 
-def test_palindrome_02():
-    assert palindrome.palindrome("A man, A Plan, A Canal, Panama") is True
+def test_palindrome_02() -> bool:
+    test = palindrome.prep_input("A man, A Plan, A Canal, Panama")
+    assert test is True
+    return test
 
 
-def test_palindrome_03():
-    assert palindrome.palindrome("Foo bar") is False
+def test_palindrome_03() -> bool:
+    test = palindrome.prep_input("Foo bar")
+    assert test is False
+    return test
 
 
-def test_palindrome_04():
-    assert palindrome.palindrome([]) is False
+def test_palindrome_04() -> bool:
+    test = palindrome.prep_input("[]")
+    assert test is False
+    return test
+
+
+def test_palindrome_05() -> bool:
+    with pytest.raises(AttributeError):
+        assert palindrome.prep_input([]) is False  # type: ignore
+        raise
