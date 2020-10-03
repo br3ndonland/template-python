cd73ae12c8aef9b854243337111b2b0ddd2b7199
Brendon Smith <br3ndonland@protonmail.com>
2020-10-03 17:36:46 -0400

    Improve package version calculation and test
    br3ndonland/inboard#8
    
    - Add version calculation using `importlib.metadata`
      https://docs.python.org/3/library/importlib.metadata.html
    - Avoid throwing error if project is not installed (such as in Docker)

--- templatepython/__init__.py
+++ templatepython/__init__.py
@@ -1,1 +1,16 @@
-__version__ = "0.1.0"
+"""
+templatepython
+---
+"""
+from importlib.metadata import version
+
+
+def package_version(package: str = __package__) -> str:
+    """Calculate version number based on pyproject.toml"""
+    try:
+        return version(package)
+    except Exception:
+        return "Package not found."
+
+
+__version__ = package_version()
--- tests/test_version.py
+++ tests/test_version.py
@@ -1,5 +1,18 @@
-from templatepython import __version__  # type: ignore
+from templatepython import __version__, package_version
+
+current_version = "0.1.0"
+
+
+def test_package_version() -> None:
+    """Test package version calculation."""
+    assert package_version() == current_version
+
+
+def test_package_version_not_found() -> None:
+    """Test package version calculation when package is not installed."""
+    assert package_version(package="incorrect") == "Package not found."
 
 
 def test_version() -> None:
-    assert __version__ == "0.1.0"
+    """Test package version number."""
+    assert __version__ == current_version
