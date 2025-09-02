98047300a31b78032d8e3351f40f3fc29a91d9fc
Brendon Smith <bws@bws.bio>
2025-09-01 23:52:46 -0400

    Set default version to `0.0.0`
    The previous default version was `__version__ = "0.1.0"`. Users of this
    template may not want to begin at `0.1.0` because they may want to start
    on a patch version first. Attempting to decrement a version with Hatch
    will raise a `ValueError`. For example, attempting `hatch version 0.0.1`
    raises "ValueError: Version `0.0.1` is not higher than the original
    version `0.1.0`".
    
    This commit will set the default version number to `0.0.0` so users of
    this template can choose their initial version number.

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,15 +1,6 @@
 import packaging.version
-import pytest
 
 import template_python
-
-
-@pytest.mark.parametrize(
-    "version,expected_version",
-    ((template_python.__version__, "0.1.0"),),
-)
-def test_version_matches_expected(version: str, expected_version: str) -> None:
-    assert version == expected_version
 
 
 def test_version_is_valid() -> None:
